94846c0ca043f592c60c8cb95590d1e6d0d746d7
Morthan666 <m.k.gozdzikiewicz@gmail.com>
2017-03-14 13:57:56 +0100

    submission method added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from models.assignment import Assignment
+from models.submission import Submission
 
 
 app = Flask(__name__)
@@ -17,9 +18,20 @@ def login():
 @app.route('/assignment')
 def assignment():
     assignments_list = Assignment.get_all()
-    return render_template(assignments_list.html, assignments = assignments_list)
+    return render_template('assignment_list.html', assignments = assignments_list)
 
-
+@app.route('/assignment/<assignment_id>/submit',methods=['GET','POST'])
+def submit(assignment_id):
+    if request.method == "POST":
+        user_id =1
+        assignment = Assignment.get_by_id(assignment_id)
+        if assignment.as_team:
+            team_id =1
+        else:
+            team_id = 'NULL'
+        Submission.save(user_id, request.form['link'],'NULL',assignment,team_id)
+        return redirect('/assignment')
+    return render_template('submit_ass.html')
 
 @app.route('/student')
 def student():
