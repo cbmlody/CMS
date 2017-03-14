02669afda23695937e60daa68201308928ea5458
michalwilk96 <michalwilk96@gmail.com>
2017-03-14 11:31:34 +0100

    Implement routes to login and main page

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template ,request
+from flask import Flask, render_template ,request, redirect, url_for
 
 
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return redirect(url_for('login'))
+
+@app.route('/login')
 def login():
     return render_template('index.html')
 
+@app.route('/main')
+def main():
+    return render_template('main.html')
 
 
 if __name__ == "__main__":
