672a1a105a9e58095ef4590e02e02185c4fe55b8
balthum <laskawiecdenis@gmail.com>
2017-03-15 17:05:29 +0100

    Deleted unused imports

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from time import strftime as stime
-from models import Assignment, Submission, Team, Student, Mentor, Checkpoint, Attendance
+from models import Assignment, Submission, Team, Student, Mentor, Attendance
 
 
 app = Flask(__name__)
