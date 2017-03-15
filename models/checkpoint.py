2f72b8e312ca8e5b74b0c00564c552cdcc9bee3f
michalwilk96 <michalwilk96@gmail.com>
2017-03-15 22:06:34 +0100

    Add checkpoint class with update user_card method

--- models/checkpoint.py
+++ models/checkpoint.py
@@ -1,18 +1,18 @@
-import sqlite3
 from models.database import Database
+
 
 class Checkpoint:
 
-    def __init__(self, id_, user_id, card):
-        self.id_ = id_
+    def __init__(self, user_id, card):
         self.user_id = user_id
         self.card = card
 
     @classmethod
-    def get_all(cls):
-        checkpoints = []
+    def update_user_card(cls, card, user_id):
         conn, cur = Database.db_connect()
-        checkpoints_list = cur.execute("SELECT * FROM `CHECKPOINTS`")
-        for check in checkpoints_list:
-            checkpoints.append(Checkpoint(*check))
-        return checkpoints
+        try:
+            cur.execute("INSERT OR IGNORE INTO `CHECKPOINTS` VALUES (?,?)", (user_id, card))
+            cur.execute("UPDATE `CHECKPOINTS` SET card=(?) WHERE user_id = (?)", (card, user_id))
+            conn.commit()
+        finally:
+            conn.close()
