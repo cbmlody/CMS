74fc1519549a866962115c3e9ca38e758a10e3f0
Cezary Broś <cezary.bros+git@gmail.com>
2017-03-28 22:50:37 +0200

    Add blank liens, Remove unnecessary docstring

--- models/attendance.py
+++ models/attendance.py
@@ -4,15 +4,14 @@ from sqlalchemy import Column, Integer, String, Boolean
 
 class Attendance(Base):
     """Holds attendance data"""
+
     __tablename__ = 'attendance'
+
     id = Column(Integer, primary_key=True, autoincrement=True)
     user_id = Column(Integer, nullable=False)
     date = Column(String(10), nullable=False)
     status = Column(Boolean, nullable=False)
 
-    """
-    Attendance which contains information of date and status of attendance
-    """
     def __init__(self, user_id, date, status):
         self.user_id = user_id
         self.date = date
