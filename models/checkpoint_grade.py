2e32beab9e8199c61dbe4defda2e41601015fffa
michalwilk96 <michalwilk96@gmail.com>
2017-03-28 12:44:15 +0200

    Implement checkpoint grades class

---
+++ models/checkpoint_grade.py
@@ -0,0 +1,17 @@
+from models.database import db_session
+from models.database import Base
+from sqlalchemy import Column, Integer, String, Boolean
+
+
+class CheckpointGrades(Base):
+    """Holds checkpoints user grades"""
+    __tablename__ = 'checkpoint_grade'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, nullable=False)
+    checkpoint_id = Column(Integer, nullable=False)
+    card = Column(String(50), nullable=False)
+
+    def __int__(self, user_id, checkpoint_id, card):
+        self.user_id = user_id
+        self.checkpoint_id = checkpoint_id
+        self.card = card
