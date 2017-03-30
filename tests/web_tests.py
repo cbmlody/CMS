d3f9611f897ad95547fd01918569975cddb5f494
Denis Łaskawiec <laskawiecdenis@gmail.com>
2017-03-30 15:30:41 +0200

    Create student feature tests

--- tests/web_tests.py
+++ tests/web_tests.py
@@ -86,6 +86,29 @@ class ccms(unittest.TestCase):
         self.driver.find_element_by_xpath("//input[@name='max-points']").send_keys("36")
         self.driver.find_element_by_xpath("//input[@type='submit']").click()
 
+    def test_e_student_features(self):
+        elem = self.driver.find_element_by_name("username")
+        elem.send_keys("Wilq")
+        elem2 = self.driver.find_element_by_name("password")
+        elem2.send_keys("1234")
+        time.sleep(1)
+        elem2.send_keys(Keys.RETURN)
+        time.sleep(1)
+        self.assertTrue('id="error"' not in self.driver.page_source)
+        self.driver.find_element_by_xpath("//a[contains(@href,'/student')]").click()
+        time.sleep(3)
+        self.driver.find_element_by_xpath("//a[contains(.,' View grades')]").click()
+        time.sleep(3)
+        self.driver.back()
+        time.sleep(2)
+        self.driver.find_element_by_xpath("//a[contains(.,' View attendance')]").click()
+        time.sleep(3)
+        self.driver.find_element_by_xpath("//a[@href='/assignment']").click()
+        self.driver.find_element_by_xpath("//a[@href='assignment/1/submit']").click()
+        self.driver.find_element_by_xpath("//input[@name='link']").send_keys("https://github.com/CodecoolKrakow20161/python-ccms-kodozercy")
+        time.sleep(5)
+        self.driver.find_element_by_xpath("//input[@class='button']").click()
+
     def tearDown(self):
         self.driver.close()
 
