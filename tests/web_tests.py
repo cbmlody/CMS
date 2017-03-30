f2ea93cae52ba992439bbfd063bc477396107805
Denis Łaskawiec <laskawiecdenis@gmail.com>
2017-03-30 15:20:16 +0200

    Add login test

--- tests/web_tests.py
+++ tests/web_tests.py
@@ -10,6 +10,16 @@ class ccms(unittest.TestCase):
         self.driver.get("http://localhost:5000")
         self.driver.maximize_window()
 
+    def test_a_admin_login(self):
+        elem = self.driver.find_element_by_name("username")
+        elem.send_keys("admin")
+        elem2 = self.driver.find_element_by_name("password")
+        elem2.send_keys("1234")
+        time.sleep(1)
+        elem2.send_keys(Keys.RETURN)
+        time.sleep(1)
+        self.assertTrue('id="error"' not in self.driver.page_source)
+
     def tearDown(self):
         self.driver.close()
 
